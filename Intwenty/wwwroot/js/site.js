d8185b4196b12a19ccbcd2b35560a9bcc7eeac10
Johan Filipsson <johan@domitor.se>
2020-02-22 17:41:31 +0100

    -

--- Intwenty/wwwroot/js/site.js
+++ Intwenty/wwwroot/js/site.js
@@ -1,4 +1,53 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿class IntwentyHelper
+{
 
-// Write your JavaScript code.
+    constructor(vuecontext, endpoint)
+    {
+        this.vuecontext = vuecontext;
+        this.endpoint = endpoint;
+    }
+
+    /*
+     * Function to retrieve a dataview value (record) 
+     * Called from views: create/open
+     * Usage: Wxwcuted when a user tabs out of an input LOOKUPKEYFIELD included in a LOOKUP
+    */
+    getDataViewValue(keyfield, lookupid, senddata) {
+
+        $("input[data-lookupid]").each(function () {
+            var id = $(this).data('lookupid');
+            if (id === lookupid) {
+                var dbfield = $(this).data('dbfield');
+                if (dbfield === keyfield) {
+                    senddata.SearchValue = this.vuecontext.model[dbfield];
+                }
+            }
+        });
+
+        if (!senddata.SearchValue)
+            return;
+
+
+        $.ajax({
+            url: this.endpoint,
+            type: "POST",
+            contentType: "application/json",
+            data: JSON.stringify(senddata),
+            success: function (response) {
+                var dataviewitem = JSON.parse(response.data);
+                $("input[data-lookupid]").each(function () {
+                    var id = $(this).data('lookupid');
+                    if (id === lookupid) {
+                        var dbfield = $(this).data('dbfield');
+                        var viewfield = $(this).data('viewfield');
+                        this.vuecontext.model[dbfield] = dataviewitem[viewfield];
+                        this.vuecontext.$forceUpdate();
+                    }
+                });
+
+
+            }
+        });
+
+    }
+}
